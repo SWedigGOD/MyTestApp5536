51d7f6342cf777b5d9b773d278c868e334307d21
SWedigGOD <>
2021-06-23 11:39:28 +0200

    next commit

---
+++ MyTestApp5536/Data/MyTestApp5536Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyTestApp5536.Models;
+
+namespace MyTestApp5536.Data
+{
+    public class MyTestApp5536Context : DbContext
+    {
+        public MyTestApp5536Context (DbContextOptions<MyTestApp5536Context> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<MyTestApp5536.Models.TestModel> TestModel { get; set; }
+    }
+}
---
+++ MyTestApp5536/Models/TestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTestApp5536.Models
+{
+    public class TestModel
+    {
+        public string Name
+        {
+            get; set;
+        }
+        public int Id
+        {
+            get; set;
+        }
+    }
+}
