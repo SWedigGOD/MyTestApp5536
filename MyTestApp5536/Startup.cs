5720f37425cb1cfe26e43adb7db2e99c93e5b34c
SWedigGOD <>
2021-06-23 14:18:17 +0200

    next commit6

--- MyTestApp5536/Startup.cs
+++ MyTestApp5536/Startup.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MyTestApp5536.Data;
+using System.Configuration;
 
 namespace MyTestApp5536
 {
@@ -31,14 +32,17 @@ namespace MyTestApp5536
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
             string conStr = "Server=mytestpostgredb5536.postgres.database.azure.com;Database=mypgsqldb;Port=5432;User Id=SWedig@mytestpostgredb5536;Password=TcvDzE8cBjVbLmy;Ssl Mode=Require;";/*"Database=mypgsqldb; Data Source=mytestpostgredb5536.postgres.database.azure.com; User Id=SWedig@mytestpostgredb5536; Password=TcvDzE8cBjVbLmy";/*String.Format(Configuration.GetConnectionString("MyTestApp5536Context"),
                     Host,
                     User,
                     DBname,
                     Port,
                     Password);*/
+            //Configuration["My:Hierarchical:Config:Data"];
+
             services.AddDbContext<MyTestApp5536Context>(options =>
-                    options.UseNpgsql(Configuration.GetConnectionString("MyDbConnection")));
+                    options.UseNpgsql(Configuration.GetConnectionString("POSTGRESQLCONNSTR_MyDbConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
