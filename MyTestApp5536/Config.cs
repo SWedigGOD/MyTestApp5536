65108f86008ba59655ae97b38af207f2a340a1bd
SWedigGOD <>
2021-06-24 15:30:55 +0200

    next commit6

---
+++ MyTestApp5536/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTestApp5536
+{
+    public static class Config
+    {
+        public static string StorageACConStr = "";
+        public static string DBConnectionString = "";
+    }
+}
